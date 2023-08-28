80fe63def1d9188613ec447a76a3a5eae28c64e3
Heri Rusmanto <mail@heri.life>
2023-08-28 20:01:41 +0700

    Add improvement

--- ugmlogin.py
+++ ugmlogin.py
@@ -6,31 +6,46 @@ from selenium import webdriver
 from selenium.webdriver.support import ui
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
-from selenium.common.exceptions import WebDriverException
+from selenium.common.exceptions import WebDriverException, NoSuchElementException, WebDriverException  
 from config import ugm_username, ugm_password
+import logging
 
-# Using ChromeDriver
-chrome_options = webdriver.ChromeOptions()
-chrome_options.add_argument('--headless')
-driver = webdriver.Chrome(chrome_options=chrome_options)
 
-# Using FirefoxDriver
-# driver = webdriver.Firefox()
+logging.basicConfig(filename='app.log', level=logging.INFO)  
 
-driver.set_window_size(800, 600)
-driver.get('https://internet.ugm.ac.id/sso/login')
+try:
+    # Using ChromeDriver
+    chrome_options = webdriver.ChromeOptions()
+    chrome_options.add_argument('--headless')
+    driver = webdriver.Chrome(chrome_options=chrome_options)
 
-#enter username
-email = driver.find_element_by_id("username")
-email.send_keys(ugm_username)
+    # Using FirefoxDriver
+    # driver = webdriver.Firefox()
 
-#enter password
-password = driver.find_element_by_id("password")
-password.send_keys(ugm_password)
+    driver.set_window_size(800, 600)
+    driver.get('https://internet.ugm.ac.id/sso/login')
 
-#login
-loginBtn = driver.find_element_by_name("submit")
-loginBtn.click()
+    #enter username
+    email = driver.find_element_by_id("username")
+    email.send_keys(ugm_username)
 
-# using this line for closing chrome driver
-driver.close()
+    #enter password
+    password = driver.find_element_by_id("password")
+    password.send_keys(ugm_password)
+    if not ugm_username:  
+        raise ValueError("Username is empty")  
+    if not ugm_password:  
+        raise ValueError("Password is empty")  
+
+    #login
+    loginBtn = driver.find_element_by_name("submit")
+    loginBtn.click()
+
+    # using this line for closing chrome driver
+    driver.close()
+    logging.info("Login successful")  
+
+except NoSuchElementException as e:  
+    logging.error("Element not found:", e)  
+except WebDriverException as e:  
+    logging.error("WebDriver error:", e)  
